30c8c0d3ce8753ed8d027d6b2f87a9aa49f65317
Sarangan <sarauniverse7@gmail.com>
2018-07-03 20:27:46 +0200

    StringUtils class with the method for finding the first non repeating character in a string

---
+++ src/main/java/com/wirecard/assignment/StringUtils.java
@@ -0,0 +1,40 @@
+package com.wirecard.assignment;
+
+import java.util.HashMap;
+
+/**
+ * Utils class for the String operations
+ */
+public class StringUtils {
+
+    /**
+     * Returns the first non repeating character in the given string word.
+     * It returns the default value of the character \u0000 when the input string is null or an empty string or when there is no non-repeating characters.
+     * Does not take into account the case of the characters. (e.g) 'B' and 'b' considered equal.
+     * @param word The input string
+     * @return The first non repeating character.
+     */
+    public static char getFirstNonRepeatingChar(String word) {
+        Character nonRepeatingCharacter = Character.MIN_VALUE;
+        if(word == null) {
+            return nonRepeatingCharacter.charValue();
+        }
+        String wordLowerCase = word.toLowerCase();
+        HashMap<Character,Integer> map = new HashMap<>();
+        for(int i=0;i<wordLowerCase.length();i++) {
+            Character ch=wordLowerCase.charAt(i);
+            Integer count = map.get(ch);
+            count = count == null ? 0 : count;
+            map.put(ch, ++count);
+        }
+        for(int i=0;i<wordLowerCase.length();i++) {
+            Character ch=wordLowerCase.charAt(i);
+            int count = map.get(ch);
+            if(count == 1) {
+                nonRepeatingCharacter=word.charAt(i);
+                break;
+            }
+        }
+        return nonRepeatingCharacter.charValue();
+    }
+}
