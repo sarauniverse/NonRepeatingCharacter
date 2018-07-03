b8d59717dd7d7eef28d681c61ba4d87694f7c32e
Sarangan <sarauniverse7@gmail.com>
2018-07-03 20:44:35 +0200

    Comments added

--- src/main/java/com/wirecard/assignment/StringUtils.java
+++ src/main/java/com/wirecard/assignment/StringUtils.java
@@ -21,12 +21,14 @@ public class StringUtils {
         }
         String wordLowerCase = word.toLowerCase();
         HashMap<Character,Integer> map = new HashMap<>();
+        // Count the occurrence of each character and record it in the HashMap
         for(int i=0;i<wordLowerCase.length();i++) {
             Character ch=wordLowerCase.charAt(i);
             Integer count = map.get(ch);
             count = count == null ? 0 : count;
             map.put(ch, ++count);
         }
+        // Get the first character with 1 occurrence.
         for(int i=0;i<wordLowerCase.length();i++) {
             Character ch=wordLowerCase.charAt(i);
             int count = map.get(ch);
