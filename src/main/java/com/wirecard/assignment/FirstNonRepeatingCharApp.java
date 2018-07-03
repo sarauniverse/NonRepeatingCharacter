488a0d1d4e6466d142fdb9228c9f56e0562ea2d3
Sarangan <sarauniverse7@gmail.com>
2018-07-03 20:28:03 +0200

    Main class for the application added

---
+++ src/main/java/com/wirecard/assignment/FirstNonRepeatingCharApp.java
@@ -0,0 +1,11 @@
+package com.wirecard.assignment;
+
+/**
+ * The main application class to demonstrate the functionality of First non repeating character in a string
+ */
+public class FirstNonRepeatingCharApp {
+
+    public static void main(String[] args) {
+        System.out.println(StringUtils.getFirstNonRepeatingChar("Sarangan"));
+    }
+}
